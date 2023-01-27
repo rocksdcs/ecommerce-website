c66bbd98d663521c52172f8da7d24a35e86bd07a
RockyCodes <117854277+rocksdcs@users.noreply.github.com>
2023-01-27 13:16:14 -0800

    notes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+//assigns const to a description set of words
 const hamburger = document.querySelector(".menu_icon");
 const sidenavContainer = document.querySelector(".sidenav_container")
 
-
+//sets the .menu_icon to toggle and work with the X button
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     sidenavContainer.classList.toggle("active");
 })
 
+//allows the .sidenavContainer to disappear when a link is clicked
 document.querySelectorAll(".sidenav_container a").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     sidenavContainer.classList.remove("active");
