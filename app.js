400fbcde2c2da4b873d7f9a3eb16c0ee22585f36
RockyCodes <117854277+rocksdcs@users.noreply.github.com>
2023-02-01 14:28:05 -0800

    cart page completion

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-//assigns const to a description set of words
 const hamburger = document.querySelector(".hamburger");
-const sidenavContainer = document.querySelector(".mobile_nav")
+const sidenavContainer = document.querySelector(".mobile_nav");
+const cartIcon = document.querySelector(".cart_icon");
+const cartPage = document.querySelector(".cart_page");
 
-//sets the .menu_icon to toggle and work with the X button
+cartIcon.addEventListener("click", () => {
+    cartIcon.classList.toggle("active");
+    cartPage.classList.toggle("active");
+})
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     sidenavContainer.classList.toggle("active");
 })
 
-//allows the .sidenavContainer to disappear when a link is clicked
 document.querySelectorAll(".mobile_nav a").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     sidenavContainer.classList.remove("active");
